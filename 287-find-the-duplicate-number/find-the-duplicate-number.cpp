e3493ca9abf05c28e998bcd5564d804a0eb01e64
SONU KUMAR <sonuk.gupta81@gmail.com>
2026-07-30 22:44:10 +0530

    Time: 63 ms (19.84%) | Memory: 87.8 MB (16.65%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int>frq;
+        for(int x :nums){
+            frq[x]++;
+            if(frq[x]>1)return x;
+        }
+        return -1;
+      
+
+        
+    }
+};
