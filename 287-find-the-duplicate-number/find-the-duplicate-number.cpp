8c6d6b2da35265e5839dbacc2c8d2282b7a0d0bd
SONU KUMAR <sonuk.gupta81@gmail.com>
2026-07-30 22:48:52 +0530

    Time: 71 ms (13.79%) | Memory: 88 MB (12.43%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int>frq;
-        for(int x :nums){
-            frq[x]++;
-            if(frq[x]>1)return x;
+        unordered_set<int>frq;
+        for(int x:nums){
+            if(frq.contains(x)){
+                return x;
+
+            }
+            frq.insert(x); 
         }
         return -1;
-      
+       
 
         
     }
