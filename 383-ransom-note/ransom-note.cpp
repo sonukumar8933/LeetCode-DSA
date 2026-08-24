611cc7c8d9a14fa6413aeb2df5535f822531667e
SONU KUMAR <sonuk.gupta81@gmail.com>
2026-08-24 09:23:03 +0530

    Time: 20 ms (5.19%) | Memory: 11.9 MB (29.93%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int>frq;
+        map<char,int>frq;
         for(char ch : magazine){
             frq[ch]++;
         }
