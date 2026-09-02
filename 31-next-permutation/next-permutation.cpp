ebefbaa935b86b76fbe23cdf8f8e36a5d1629553
SONU KUMAR <sonuk.gupta81@gmail.com>
2026-09-02 21:27:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (15.12%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int i;
+        for( i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                break;}
+        }
+        if(i>=0){
+            for(int j=n-1;j>i;j--){
+                if(nums[j]>nums[i]){
+                    swap(nums[j],nums[i]);
+                    break;
+                }
+            }
+
+        }
+        reverse(nums.begin()+i+1,nums.end());
+        
+    }
+};
